aa43f8d54486dd1f91845b7d4343366d2aa3f3dc
Petya Peeva <Petya.Peeva@kontrax.bg>
2023-04-12 16:47:30 +0300

    Added ImportController with Test method to echo JSON compatible data.

---
+++ ExcelImporter.Web/Controllers/ImportController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelImporter.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        public ImportController()
+        {
+        }
+
+        /// <summary>
+        /// Test posting data. 
+        /// </summary>
+        /// <param name="data">Any object convertible to JSON.</param>
+        /// <returns>Message for success and echoed data.</returns>
+        [HttpPost("test")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Test([FromBody] object data)
+        {
+            try
+            {
+                if (data == null)
+                {
+                    return BadRequest("Missing data!");
+                }
+
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                string result = $"Test succeeded! data: {json}";
+
+                return Ok(result);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error on import test");
+            }
+        }
+
+
+    }
+}
--- ExcelImporter.Web/Program.cs
+++ ExcelImporter.Web/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configures Newtonsoft.Json specific features such as input and output formatters.
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
